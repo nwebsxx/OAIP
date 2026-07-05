d1ecd1c6700ca5fd640ff2d2db2b5d7a81e2af64
nwebsxx <bakacevaanastasia@gmail.com>
2026-07-05 23:50:27 +0300

    Работа с файлами на языке С++

---
+++ lab2/5.1/5.1/5.1.cpp
@@ -0,0 +1,58 @@
+﻿// Скопировать в файл FILE2 только четные строки из FILE1. 
+// Подсчитать размер файлов FILE1 и FILE2 (в байтах).
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	ifstream fin;
+	fin.open("D:\\unik\\2sem\\oaip\\lab2\\FILE1.txt");
+
+	ofstream fout;
+	fout.open("FILE2.txt");
+
+	if (!fin.is_open()) {
+		cout << "Ошибка" << endl;
+		return 1;
+	}
+
+	string str;
+	int k = 1;
+	while (getline(fin, str)) {
+		if (k % 2 == 0) {
+			fout << str << endl;
+		}
+		k++;
+	}
+	fin.close();
+	fout.close();
+
+	ifstream file1("D:\\unik\\2sem\\oaip\\lab2\\FILE1.txt", ios::binary);
+
+	if (!file1.is_open()) {
+		cout << "Ошибка" << endl;
+		return 1;
+	}
+
+	file1.seekg(0, ios::end);
+	int size1 = file1.tellg();
+	file1.close();
+	cout << "Размер FILE1: " << size1 << " байт" << endl;
+
+	ifstream file2("D:\\unik\\2sem\\oaip\\lab2\\5.1\\5.1\\FILE2.txt", ios::binary);
+
+	if (!file2.is_open()) {
+		cout << "Ошибка" << endl;
+		return 1;
+	}
+
+	file2.seekg(0, ios::end);
+	int size2 = file2.tellg();
+	file2.close();
+	cout << "Размер FILE2: " << size2 << " байт" << endl;
+
+	return 0;
+}
---
+++ lab2/5.2/5.2/5.2.cpp
@@ -0,0 +1,49 @@
+﻿// Ввести с клавиатуры строку символов, состоящую из цифр и слов, разделенных пробелами, и записать ее в файл. 
+// Прочитать из файла данные и вывести нечетные числа строки. 
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int write(ofstream& f, string str);
+int read(ifstream& f, string str);
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	string str;
+	cout << "Введите строку: " << endl;
+	getline(cin ,str);
+
+	ofstream fout;
+	write(fout, str);
+
+	ifstream fin;
+	read(fin, str);
+
+	for (int i = 0; i < str.length(); i++) {
+		char c = str[i];
+		int n = c - '0';
+		if (c >= '0' && c <= '9' && n % 2 != 0) {
+			cout << "Нечетное число в строке: " << n << endl;
+		}
+	}
+	return 0;
+}
+
+int write(ofstream& f, string str)
+{
+	f.open("file3.txt");
+	f << str;
+	f.close();
+	return 0;
+}
+
+int read(ifstream& f, string str)
+{
+	f.open("file3.txt");
+	getline(f, str);
+	f.close();
+	return 0;
+}
